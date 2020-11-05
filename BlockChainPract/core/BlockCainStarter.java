9b560b07607e1cbe82595caa6184c70199ad05c0
JongSeokRyu <64138598+JongSeokRyu@users.noreply.github.com>
2020-11-06 00:56:38 +0900

    first commit

---
+++ BlockChainPract/core/BlockCainStarter.java
@@ -0,0 +1,21 @@
+package BlockChainPract.core;
+
+import BlockChainPract.util.Util;
+
+public class BlockCainStarter {
+
+	public static void main(String[] args) {
+		
+		int nonce = 0;
+		while(true) {
+			if(Util.getHash(nonce + "").substring(0,6).equals("000000")) {
+				System.out.println("몇번째? " + nonce);
+				System.out.println("해시값 : " + Util.getHash(nonce + ""));
+				break;
+			}
+			nonce++;
+		}
+
+	}
+
+}
